069dab977a8dc1ab87cd2a7e19128b24ebe1f738
firuza <firya87@gmail.com>
2020-01-07 16:46:29 +0530

    Added fields to store netlist and simulation output text

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -6,6 +6,8 @@ class Simulations(models.Model):
     name = models.CharField(max_length=50)
     cirfile_link = models.FileField()
     outfile_link = models.FileField()
+    netlist_content = models.TextField(null=True)
+    simulation_output_text = models.TextField(null=True)
     created_by = models.ForeignKey(User, null=True)
     created_on = models.DateTimeField(null=True)
 
